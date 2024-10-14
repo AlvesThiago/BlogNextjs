f487d2bd184f2ed735f1adb7a17dacc974131646
AlvesThiago <thiagodesign.27@gmail.com>
2024-10-13 23:42:52 -0300

    Configurando Kinde Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
   return (
-    <div>
+    <div className="p-10">
       <h1>Hello Word</h1>
-      <Button>Helo from the button</Button>
+
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ): (
+        <div>
+          <RegisterLink>
+            <Button>Register</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Login</Button>
+          </LoginLink>
+        </div>
+        
+      )}
     </div>
   );
 }
